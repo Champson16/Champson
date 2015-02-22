fcd83842b7e3940fc31718439403e4020e52f7ed
Terry Schussler <tschussler@fatredcouch.com>
2015-02-21 16:48:12 -0800

    U of Chew 1.00.06
    Removed com.android.vending.BILLING permission request which was flagging the app has using IAP when it doesn't.
    
    Signed-off-by: Terry Schussler <tschussler@fatredcouch.com>

--- config.lua
+++ config.lua
@@ -15,7 +15,7 @@ application = {
 
   license = {
     google = {
-      -- key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtABnCZH+DUUjLlehfutjRoPu8mO2B6A50mVofm6EVDq0Itd7yi8eAyLn/NDfAkUCKofsmdG78cLHbVjByKhyw/NhZx0ipccdLnylrrnXsIMVcyoSgWX4qULp0jdObU6zEYsIbZGVqsMxgLJqx1BixFmFE0wiKBj8hzVYZj9UEbcjXyfH/yMUU1/YRoDPTzzwgnweK8oqrSncg3Mb5I/8Z+N9SWd7LHEkaEDnuNVxjjwt8jN+Z5efoqIMgyZYDVQz5PcMhAIcPKUpej//HIHm6ilgZmyt86CcS8w/HnYU1406vKxeidok1P72ES5+1EQG2Q8rKcJZaVTTKYrPxHNNCwIDAQAB",
+      key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtABnCZH+DUUjLlehfutjRoPu8mO2B6A50mVofm6EVDq0Itd7yi8eAyLn/NDfAkUCKofsmdG78cLHbVjByKhyw/NhZx0ipccdLnylrrnXsIMVcyoSgWX4qULp0jdObU6zEYsIbZGVqsMxgLJqx1BixFmFE0wiKBj8hzVYZj9UEbcjXyfH/yMUU1/YRoDPTzzwgnweK8oqrSncg3Mb5I/8Z+N9SWd7LHEkaEDnuNVxjjwt8jN+Z5efoqIMgyZYDVQz5PcMhAIcPKUpej//HIHm6ilgZmyt86CcS8w/HnYU1406vKxeidok1P72ES5+1EQG2Q8rKcJZaVTTKYrPxHNNCwIDAQAB",
       -- The "policy" key is optional. Its value can be either "serverManaged" (default) or "strict".
       -- A value of "serverManaged" will query the Google server and cache the results (this is similar to Google's "ServerManagedPolicy").
       -- A value of "strict" will not cache the results, so when there's a network failure, the licensing will fail (this is similar to Google's "StrictPolicy").
